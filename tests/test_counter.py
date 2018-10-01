63dddbc610a064d4b7973b31066b6d6a1152a432
Manuel Vázquez Acosta <manuel@merchise.org>
2018-10-01 17:39:51 -0400

    Implement the PNCounter.

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -9,7 +9,7 @@
 from hypothesis import strategies as st
 from hypothesis.stateful import Bundle, RuleBasedStateMachine, rule
 
-from xotl.crdt.counter import GCounter
+from xotl.crdt.counter import GCounter, PNCounter
 
 
 class ModelCounter:
@@ -20,11 +20,14 @@ class ModelCounter:
         self.value += 1
         return self.value
 
+    def decr(self):
+        self.value -= 1
+
     def __repr__(self):
         return "<ModelCounter: {value}>".format(value=self.value)
 
 
-class CounterComparison(RuleBasedStateMachine):
+class GCounterComparison(RuleBasedStateMachine):
     def __init__(self):
         super().__init__()
         # The 'subject' is the thing we're testing, the model keeps track of
@@ -32,6 +35,48 @@ class CounterComparison(RuleBasedStateMachine):
         self.model = ModelCounter()
         # TODO: Replace all subjects for replicas of a distributed counter.
         self.subjects = (GCounter(actor='A'), GCounter(actor='B'))
+
+    replicas = Bundle('replicas')
+    process_names = st.sampled_from([0, 1])
+
+    @rule(target=replicas, name=process_names)
+    def replica(self, name):
+        return self.subjects[name]
+
+    @rule(replica=replicas)
+    def run_incr(self, replica):
+        value = replica.value
+        replica.incr()
+        assert value + 1 == replica.value
+        self.model.incr()
+
+    @rule(receiver=replicas)
+    def run_synchronize(self, receiver):
+        # The merge is a "local" operation.  The receiver may alter its the
+        # state, the sender state is unaltered.  But we have only two
+        # replicas, and the rule command always updates the model counter,
+        # which means that after merging, the receiver must have the same
+        # value as the model.
+        if self.subjects[0] is not receiver:
+            sender = self.subjects[0]
+        else:
+            sender = self.subjects[1]
+        state = sender.state
+        receiver.merge(state)
+        assert receiver.value == self.model.value
+
+
+TestGCounter = GCounterComparison.TestCase
+
+
+class PNCounterComparison(RuleBasedStateMachine):
+    def __init__(self):
+        super().__init__()
+        # The 'subject' is the thing we're testing, the model keeps track of
+        # what the subject state should be.
+        self.model = ModelCounter()
+        # TODO: Replace all subjects for replicas of a distributed counter.
+        self.subjects = (PNCounter(actor='A'), PNCounter(actor='B'))
 
     commands = Bundle('commands')
     counter_commands = st.sampled_from(['incr', ])
@@ -54,6 +99,11 @@ class CounterComparison(RuleBasedStateMachine):
             replica.incr()
             assert value + 1 == replica.value
             self.model.incr()
+        elif cmd == 'decr':
+            value = replica.value
+            replica.decr()
+            assert value - 1 == replica.value
+            self.model.decr()
         else:
             assert False
 
@@ -73,4 +123,4 @@ class CounterComparison(RuleBasedStateMachine):
         assert receiver.value == self.model.value
 
 
-TestCounter = CounterComparison.TestCase
+TestPNCounter = PNCounterComparison.TestCase
--- xotl/crdt/counter.py
+++ xotl/crdt/counter.py
@@ -17,7 +17,7 @@ class GCounter(CvRDT):
         self.vclock = VClock()
 
     def __repr__(self):
-        return f"<Counter of {self.value}; in {self.actor} with {self.vclock}>"
+        return f"<GCounter of {self.value}; in {self.actor} with {self.vclock}>"
 
     def incr(self):
         self.vclock = self.vclock.bump(self.actor)
@@ -26,10 +26,45 @@ class GCounter(CvRDT):
     def value(self):
         return sum(d.counter for d in self.vclock.dots)
 
-    def merge(self, other: 'Counter'):
+    def merge(self, other: 'GCounter'):
         'Merge this replica with another in-place'
-        self.vclock = self.vclock.merge(other.vc)
+        self.vclock = self.vclock.merge(other.vclock)
 
     @property
     def state(self):
         return self
+
+    def __le__(self, other):
+        return self.vclock <= other.vclock
+
+
+class PNCounter(CvRDT):
+    def __init__(self, *, actor: str):
+        self.actor = actor
+        self.pos = GCounter(actor=actor)
+        self.neg = GCounter(actor=actor)
+
+    def __repr__(self):
+        return f"<PNCounter of {self.value}; with {self.pos} and {self.neg}>"
+
+    def incr(self):
+        self.pos.incr()
+
+    def decr(self):
+        self.neg.incr()
+
+    @property
+    def value(self):
+        return self.pos.value - self.neg.value
+
+    def merge(self, other: 'PNCounter'):
+        'Merge this replica with another in-place'
+        self.pos.merge(other.pos)
+        self.neg.merge(other.neg)
+
+    @property
+    def state(self):
+        return self
+
+    def __le__(self, other):
+        return self.pos <= other.pos and self.neg <= other.neg
