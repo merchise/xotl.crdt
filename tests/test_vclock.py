7f431b53b18a6c3ea7dba700aa56929afea7cd63
Manuel Vázquez Acosta <manuel@merchise.org>
2018-10-02 20:42:59 -0400

    Add failing eq tests for vector clocks.

--- tests/test_vclock.py
+++ tests/test_vclock.py
@@ -9,17 +9,21 @@
 from xotl.crdt.clocks import VClock, Dot
 
 
-def test_descend_regression():
+def test_descend_regression1():
     v1 = VClock(dots=(Dot(actor='R0', counter=1, timestamp=0),
                       Dot(actor='R1', counter=1, timestamp=0)))
     v2 = VClock(dots=(Dot(actor='R0', counter=1, timestamp=0),))
     assert v1 >= v2
 
+
+def test_descend_regression2():
     v1 = VClock(dots=(Dot(actor='R0', counter=1, timestamp=0),
                       Dot(actor='R1', counter=1, timestamp=0)))
     v2 = VClock(dots=(Dot(actor='R1', counter=1, timestamp=0),))
     assert v1 >= v2
 
+
+def test_descend_regression3():
     v1 = VClock(dots=(Dot(actor='R0', counter=1, timestamp=0),
                       Dot(actor='R1', counter=1, timestamp=0),
                       Dot(actor='R2', counter=0, timestamp=0)))
@@ -32,3 +36,13 @@ def test_missing_present_dots_regression():
     v2 = VClock(dots=(Dot(actor='R1', counter=1, timestamp=0),))
     assert v1 <= v2
     assert not (v1 >= v2)
+
+
+def test_eq_of_empties1():
+    v1 = VClock(dots=(Dot(actor='R0', counter=0, timestamp=0),))
+    v2 = VClock(dots=(Dot(actor='R1', counter=0, timestamp=0),))
+
+    assert v1 >= v2 >= v1
+    assert v1 == v2
+    assert v2 <= v1
+    assert v1 <= v2
